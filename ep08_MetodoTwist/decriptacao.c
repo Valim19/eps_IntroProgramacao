51d9d8014c1690a6cf63d971aeb08550cba6a729
Anna Valim <annacarolinavalim@gmail.com>
2024-11-14 14:16:26 -0300

    adicionando dois métodos

--- ep08_MetodoTwist/decriptacao.c
+++ ep08_MetodoTwist/decriptacao.c
@@ -1,1 +1,63 @@
+#include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+
+void readFile(const char *fileName) {
+    FILE *file = fopen(fileName, "r");
+
+    if (file == NULL) {
+        printf("Erro! Não foi possível abrir o arquivo %s.\n", fileName);
+    }
+
+    char linha[100];
+
+    while (fgets(linha, sizeof(linha), file) != NULL) {
+        printf("%s", linha);
+    }
+
+    fclose(file);
+}
+
+void writeFile(const char *fileName) {
+    FILE *file = fopen(fileName, "w");
+
+    if (file == NULL) {
+        printf("Erro! Não foi possível abrir o arquivo %s. \n", fileName);
+    }
+
+    char *linhas[] = {
+        "Esta é a primeira linha.\n",
+        "Esta é a segunda  linha.\n",
+        "Esta é a terceira linha.\n"
+    };
+
+    for(int i = 0; i < 3; i++) {
+        fputs(linhas[i], file);
+    }
+        
+    fclose(file);
+}
+
+void plainText() {
+
+}
+
+void plainCode() {
+
+}
+
+void cipherCode() {
+
+}
+
+void cipherText() {
+
+}
+
+int main() {
+    readFile("ciphertext.txt");
+
+    writeFile("plaintext.txt");
+
+    return 0;
+}
